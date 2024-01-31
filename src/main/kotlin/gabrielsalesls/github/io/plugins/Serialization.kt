5327e5513007a5e977bfe548d446d70be5804854
gabrielsalesls <salesgabriel311@gmail.com>
2024-01-31 11:05:35 -0300

    feat: Serialization config

--- src/main/kotlin/gabrielsalesls/github/io/plugins/Serialization.kt
+++ src/main/kotlin/gabrielsalesls/github/io/plugins/Serialization.kt
@@ -1,15 +1,11 @@
 package gabrielsalesls.github.io.plugins
 
-import io.ktor.serialization.gson.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        gson {
-            setPrettyPrinting()
-        }
+        json()
     }
 }
